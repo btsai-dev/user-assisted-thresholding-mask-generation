1a244857b47ec85849250e656296aad47d61a531
godonan <godonanabt@gmail.cm>
2020-07-08 10:53:47 -0500

    Separated Image Processing
    Separated thread executions
    Added mask creation interruptions

--- src/main/java/sample/DisplayController.java
+++ src/main/java/sample/DisplayController.java
@@ -3,10 +3,12 @@ package sample;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import org.opencv.core.*;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
@@ -42,21 +44,57 @@ public class DisplayController {
     Slider Thresh002;
 
     @FXML
-    Button LOAD;
+    Button LOADBLUR;
+
+    @FXML
+    Button LOADCANY;
+
+    @FXML
+    Button LOADMASK;
 
     @FXML
     Button SAVE;
 
+    @FXML
+    GridPane IMGDISP;
+
+    @FXML
+    Button INTERRUPT;
+
     private static File imgFile;
+    private static final int ROWS = 2;
+    private static final int COLS = 2;
+    private static final int MARGIN = 10;
+    private static Mat orig;
+    private static Mat gray;
+    private static Mat blur;
+    private static Mat cany;
+    private static Mat mask;
+    private static String stage = "NONE";
+    private static Thread worker;
 
     public void initData(Image img, File file){
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        orig = new Mat();
+        gray = new Mat();
+        blur = new Mat();
+        cany = new Mat();
+        mask = new Mat();
+
         SAVE.setDisable(true);
         ORIG.setImage(img);
         BLUR.setImage(null);
         CANY.setImage(null);
         MASK.setImage(null);
-
+        IMGDISP.setAlignment(Pos.CENTER);
+        ORIG.fitHeightProperty().bind(IMGDISP.heightProperty().subtract(MARGIN).divide(ROWS));
+        ORIG.fitWidthProperty().bind(IMGDISP.widthProperty().subtract(MARGIN).divide(COLS));
+        BLUR.fitHeightProperty().bind(IMGDISP.heightProperty().subtract(MARGIN).divide(ROWS));
+        BLUR.fitWidthProperty().bind(IMGDISP.widthProperty().subtract(MARGIN).divide(COLS));
+        CANY.fitHeightProperty().bind(IMGDISP.heightProperty().subtract(MARGIN).divide(ROWS));
+        CANY.fitWidthProperty().bind(IMGDISP.widthProperty().subtract(MARGIN).divide(COLS));
+        MASK.fitHeightProperty().bind(IMGDISP.heightProperty().subtract(MARGIN).divide(ROWS));
+        MASK.fitWidthProperty().bind(IMGDISP.widthProperty().subtract(MARGIN).divide(COLS));
         imgFile = file;
     }
 
@@ -64,31 +102,47 @@ public class DisplayController {
 
     }
 
-    public void loadMask(ActionEvent actionEvent) {
-        // Disable all the buttons
-        SAVE.setDisable(true);
-        LOAD.setDisable(true);
-        new Thread(){
-            public void run(){
-                System.out.println("Executing thread.");
+    public void interrupt(ActionEvent actionEvent) {
+        System.out.println("Interrupted Thread");
+        if (stage.equals("NONE")){
+            System.out.println("No stage detected.");
+            return;
+        }
+        try {
+            worker.interrupt();
+        } catch(Exception e){
+            System.out.println("Error when stopping Thread.");
+            e.printStackTrace();
+        }
+        switch (stage){
+            case "BLUR":
+                disableAllButtons();
+                LOADBLUR.setDisable(false);
+                break;
+            case "CANY":
+                disableAllButtons();
+                LOADBLUR.setDisable(false);
+                LOADCANY.setDisable(false);
+                break;
+            case "MASK":
+                disableAllButtons();
+                LOADBLUR.setDisable(false);
+                LOADCANY.setDisable(false);
+                LOADMASK.setDisable(false);
+                break;
+        }
+    }
+
+    public void loadBlur(ActionEvent actionEvent) {
+        disableAllButtons();
+        worker = new Thread(){
+            public void run() {
+                System.out.println("Executing Blur Thread.");
                 int filterSize = (int) ThreshSize.getValue();
                 int filterSigma = (int) ThreshSigma.getValue();
-                int threshold001 = (int) Thresh001.getValue();
-                int threshold002 = (int) Thresh002.getValue();
-                int threshHigh = Math.max(threshold001, threshold002);
-                int threshLow = Math.min(threshold001, threshold002);
-                int kernelSize = 3;
-
-                Mat orig = new Mat();
-                Mat gray = new Mat();
-                Mat blur = new Mat();
-                Mat cany = new Mat();
-                // Mat mask = new Mat();
-                //Mat mask = new Mat(orig.size(), CvType.CV_8UC3, Scalar.all(0));
-
 
                 orig = Imgcodecs.imread(DisplayController.imgFile.getAbsolutePath(), Imgcodecs.IMREAD_COLOR);
-                if( orig.empty() ) {
+                if (orig.empty()) {
                     System.out.println("Error opening image");
                     System.out.println(imgFile.getAbsolutePath());
                     return;
@@ -100,7 +154,37 @@ public class DisplayController {
                 Imgcodecs.imencode(".bmp", blur, blurMatByte);
                 Image blurImage = new Image(new ByteArrayInputStream(blurMatByte.toArray()));
 
-                Imgproc.Canny(blur, cany, threshLow, threshHigh, kernelSize, false);
+                Platform.runLater(new Runnable() {
+                    public void run() {
+                        System.out.println("Updating Blur UI.");
+                        BLUR.setImage(blurImage);
+                        INTERRUPT.setDisable(true);
+                        LOADBLUR.setDisable(false);
+                        LOADCANY.setDisable(false);
+                        stage = "NONE";
+                    }
+                });
+            }
+        };
+        worker.start();
+        stage = "BLUR";
+        INTERRUPT.setDisable(false);
+    }
+
+
+    public void loadCany(ActionEvent actionEvent) {
+        disableAllButtons();
+        worker = new Thread(){
+            public void run() {
+                System.out.println("Executing Canny Thread.");
+
+                int threshold001 = (int) Thresh001.getValue();
+                int threshold002 = (int) Thresh002.getValue();
+                int threshHigh = Math.max(threshold001, threshold002);
+                int threshLow = Math.min(threshold001, threshold002);
+                int kernelSize = 3;
+
+                Imgproc.Canny(blur, cany, threshLow, threshHigh, kernelSize, true);
                 cany.convertTo(cany, CvType.CV_8UC1);
                 MatOfByte canyMatByte = new MatOfByte();
                 Imgcodecs.imencode(".bmp", cany, canyMatByte);
@@ -108,19 +192,48 @@ public class DisplayController {
 
                 Platform.runLater(new Runnable() {
                     public void run() {
-                        System.out.println("Updating UI.");
-                        BLUR.setImage(blurImage);
+                        System.out.println("Updating Canny UI.");
                         CANY.setImage(canyImage);
+                        INTERRUPT.setDisable(true);
+                        LOADBLUR.setDisable(false);
+                        LOADCANY.setDisable(false);
+                        LOADMASK.setDisable(false);
+                        stage = "NONE";
                     }
                 });
+            }
+        };
+        worker.start();
+        stage = "CANY";
+        INTERRUPT.setDisable(false);
+    }
 
+    private void disableAllButtons() {
+        SAVE.setDisable(true);
+        LOADBLUR.setDisable(true);
+        LOADCANY.setDisable(true);
+        LOADMASK.setDisable(true);
+        INTERRUPT.setDisable(true);
+    }
+
+    public void loadMask(ActionEvent actionEvent) {
+        disableAllButtons();
+        worker = new Thread(){
+            @Override
+            public void run(){
+                System.out.println("Executing Mask Thread.");
                 List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
                 // Imgproc.findContours(cany, contours, new Mat(), Imgproc.RETR_FLOODFILL, Imgproc.CHAIN_APPROX_SIMPLE);
                 Imgproc.findContours(cany, contours, new Mat(), Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
-                Mat mask = new Mat(orig.size(), orig.type(), Scalar.all(0));
+                mask = new Mat(orig.size(), orig.type(), Scalar.all(0));
 
                 System.out.printf("Starting contour loop. Expect %d iterations.\n", contours.size());
                 for (int i = 0; i < contours.size(); i++) {
+                    if (Thread.interrupted()) {
+                        System.out.println("Interrupted.");
+                        return;
+                    }
+                    System.out.println("Contour execute.");
                     Imgproc.drawContours(mask, contours, i, new Scalar(255, 255, 255), -1);
                 }
                 System.out.println("Ending contour loop.");
@@ -133,15 +246,19 @@ public class DisplayController {
                 Platform.runLater(new Runnable() {
                     public void run() {
                         System.out.println("Updating UI.");
-                        // BLUR.setImage(blurImage);
-                        // CANY.setImage(canyImage);
                         MASK.setImage(maskImage);
-                        LOAD.setDisable(false);
+                        INTERRUPT.setDisable(true);
+                        LOADBLUR.setDisable(false);
+                        LOADCANY.setDisable(false);
+                        LOADMASK.setDisable(false);
                         SAVE.setDisable(false);
+                        stage = "NONE";
                     }
                 });
             }
-        }.start();
+        };
+        worker.start();
+        stage = "MASK";
+        INTERRUPT.setDisable(false);
     }
-
 }
